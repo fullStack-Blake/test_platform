6b816210400cf5c33b9669263b98d51a8adc5799
Blake Han <han.sangyeup@gmail.com>
2021-12-15 01:32:51 -0500

    Test deploy without Components

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
-import Feed from "./Feed.jsx";
-import Detail from "./Detail.jsx";
-import Archive from "./Archive.jsx";
+// import Feed from "./Feed.jsx";
+// import Detail from "./Detail.jsx";
+// import Archive from "./Archive.jsx";
 import Tab from "./Tab.jsx";
 import Header from "./Header.jsx";
 
@@ -11,9 +11,9 @@ const Router = () => (
     <HashRouter>
       <Header />
       <Switch>
-        <Route path="/" exact component={Feed} />
-        <Route path="/archive" exact component={Archive} />
-        <Route path="/detail/:id" exact component={Detail} />
+        {/* <Route path="/" exact component={Feed} /> */}
+        {/* <Route path="/archive" exact component={Archive} /> */}
+        {/* <Route path="/detail/:id" exact component={Detail} /> */}
         <Redirect path="*" to="/" />
       </Switch>
     </HashRouter>
