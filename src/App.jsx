2ee8db5ce72ce4c8dc095f53a116fb49d9d3bc0a
Blake Han <han.sangyeup@gmail.com>
2021-12-15 01:39:17 -0500

    Add Router to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import ReactDOM from "react-dom";
-// import Router from "./Router.jsx";
+import Router from "./Router.jsx";
 // import Header from "./Header.jsx";
 
 const App = () => {
