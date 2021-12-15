798a2a563d0306037167657c7efbfb73ecae7e3f
Blake Han <han.sangyeup@gmail.com>
2021-12-15 01:20:13 -0500

    Edit Entry Point

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("app"));
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import './css/body.css';
-import './css/app.css';
-import './css/header.css';
+import "./css/body.css";
+import "./css/app.css";
+import "./css/header.css";
 
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
 
+ReactDOM.render(<App />, document.getElementById("app"));
