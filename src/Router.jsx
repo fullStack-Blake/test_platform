7836399cb7c72ef7c5149ebab338a12a030f61cc
Blake Han <han.sangyeup@gmail.com>
2021-12-15 02:05:12 -0500

    Remove extra imports

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
 import Header from "./Header.jsx";
+
 import Feed from "./Feed.jsx";
-import Archive from "./Archive.jsx";
-import Detail from "./Detail.jsx";
+// import Archive from "./Archive.jsx";
+// import Detail from "./Detail.jsx";
 
 const Router = () => (
   <div>
@@ -11,8 +12,8 @@ const Router = () => (
       <Header />
       <Switch>
         <Route path="/" exact component={Feed} />
-        <Route path="/archive" exact component={Archive} />
-        <Route path="/detail/:id" exact component={Detail} />
+        {/* <Route path="/archive" exact component={Archive} /> */}
+        {/* <Route path="/detail/:id" exact component={Detail} /> */}
         <Redirect path="*" to="/" />
       </Switch>
     </HashRouter>
