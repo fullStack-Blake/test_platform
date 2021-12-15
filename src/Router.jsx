c99798f57bd8875819649ddd6f9e6b6eab172e16
Blake Han <han.sangyeup@gmail.com>
2021-12-15 01:42:17 -0500

    Remove unnecessary import

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
-// import Feed from "./Feed.jsx";
-// import Detail from "./Detail.jsx";
-// import Archive from "./Archive.jsx";
-import Tab from "./Tab.jsx";
 import Header from "./Header.jsx";
+import Feed from "./Feed.jsx";
+import Archive from "./Archive.jsx";
+import Detail from "./Detail.jsx";
 
 const Router = () => (
   <div>
     <HashRouter>
       <Header />
       <Switch>
-        {/* <Route path="/" exact component={Feed} /> */}
-        {/* <Route path="/archive" exact component={Archive} /> */}
-        {/* <Route path="/detail/:id" exact component={Detail} /> */}
+        <Route path="/" exact component={Feed} />
+        <Route path="/archive" exact component={Archive} />
+        <Route path="/detail/:id" exact component={Detail} />
         <Redirect path="*" to="/" />
       </Switch>
     </HashRouter>
